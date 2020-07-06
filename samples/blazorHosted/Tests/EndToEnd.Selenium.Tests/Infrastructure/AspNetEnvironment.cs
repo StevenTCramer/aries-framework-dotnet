f933d795df92b3b60560c7199ed7aae067d79db5
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-07-06 09:41:54 +0000

    Add back to my repo the Blazor Sample stuff
    It will not be in the PR can cherty pick what I want

---
+++ samples/blazorHosted/Tests/EndToEnd.Selenium.Tests/Infrastructure/AspNetEnvironment.cs
@@ -0,0 +1,8 @@
+﻿namespace Hyperledger.Aries.AspNetCore.EndToEnd.Tests.Infrastructure
+{
+  public enum AspNetEnvironment
+  {
+    Development,
+    Production
+  }
+}
---
+++ samples/blazorHosted/Tests/EndToEnd.Selenium.Tests/Infrastructure/ClientLoaderTestConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace Hyperledger.Aries.AspNetCore.EndToEnd.Tests.Infrastructure
+{
+  using System;
+  using Hyperledger.Aries.AspNetCore.Features.ClientLoaders;
+
+  public class TestClientLoaderConfiguration : IClientLoaderConfiguration
+  {
+    public TimeSpan DelayTimeSpan => TimeSpan.FromMilliseconds(10);
+  }
+}
